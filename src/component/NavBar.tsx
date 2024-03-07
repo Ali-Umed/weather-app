2685380903367a3aded25f25c548bc72deab4fdc
Ali-Umed <aliumed844@gmail.com>
2024-03-07 11:24:06 +0300

    change style navbar and responsive to device

--- src/component/NavBar.tsx
+++ src/component/NavBar.tsx
@@ -24,7 +24,7 @@ export default function NavBar({
               data-collapse-toggle="navbar-search"
               aria-controls="navbar-search"
               aria-expanded="false"
-              className="sm:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1"
+              className=" text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1"
             >
               <svg
                 className="w-5 h-5"
